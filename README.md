2fe123ee5a8f19e13c12af7e16b18ef6c51832c2
michael-grunder <michael.grunder@gmail.com>
2019-11-21 13:47:36 -0800

    Update example to peek or not peek

--- README.md
+++ README.md
@@ -3,5 +3,9 @@ A simple project to cause a SIGSEGV in iou-rs
 Simply run like so:
 
 ```bash
-cargo run -- 3 2
+# Cause a SIGSEGV
+cargo run -- 3 2 1
+
+# Block forever failing to read the final write
+cargo run -- 3 2 0
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs::File, io::IoSlice, os::unix::io::AsRawFd};
 
-fn io_read_write(writes: u64, qd: u32) {
+fn io_read_write(writes: u64, qd: u32, use_peek: bool) {
     let mut iou = iou::IoUring::new(qd).unwrap();
 
     let file = File::create("rust_testfile").unwrap();
@@ -39,31 +39,49 @@ fn io_read_write(writes: u64, qd: u32) {
         );
     }
 
-    println!("");
-    while let Some(cqe) = iou.peek_for_cqe() {
-        let obytes = cqe.result().unwrap();
-        bytesin += obytes;
-        nreads += 1;
-        counter -= 1;
-        println!(
-            "[{}] OP {:05} ({:03} bytes) | writes: {:02} ({:03} bytes) | reads: {:02} ({:03} bytes)",
-            counter, "READ", obytes, nwrites, bytesout, nreads, bytesin
-        );
+    if use_peek {
+        // This causes a SIGSEGV for me using iou master
+        while let Some(cqe) = iou.peek_for_cqe() {
+            let obytes = cqe.result().unwrap();
+            bytesin += obytes;
+            nreads += 1;
+            counter -= 1;
+            println!(
+                "[{}] OP {:05} ({:03} bytes) | writes: {:02} ({:03} bytes) | reads: {:02} ({:03} bytes)",
+                counter, "READ", obytes, nwrites, bytesout, nreads, bytesin
+            );
+        }
+    } else {
+        // This blocks without reading the final write
+        while nreads < nwrites {
+            let cqe = iou.wait_for_cqe().unwrap();
+            let obytes = cqe.result().unwrap();
+            bytesin += obytes;
+            nreads += 1;
+            counter -= 1;
+            println!(
+                "[{}] OP {:05} ({:03} bytes) | writes: {:02} ({:03} bytes) | reads: {:02} ({:03} bytes)",
+                counter, "READ", obytes, nwrites, bytesout, nreads, bytesin
+            );
+        }
     }
 }
 
+fn get_arg<T>(n: usize, default: &str) -> T
+where
+    T: std::str::FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    std::env::args()
+        .nth(n)
+        .unwrap_or(default.to_string())
+        .parse::<T>()
+        .expect("Can't parse argument")
+}
+
 fn main() {
-    let writes = std::env::args()
-        .nth(1)
-        .unwrap_or("0".to_string())
-        .parse::<u64>()
-        .unwrap();
+    let (writes, qd, use_peek): (u64, u32, u32) =
+        (get_arg(1, "3"), get_arg(2, "2"), get_arg(3, "0"));
 
-    let qd = std::env::args()
-        .nth(2)
-        .unwrap_or("0".to_string())
-        .parse::<u32>()
-        .unwrap();
-
-    io_read_write(writes, qd);
+    io_read_write(writes, qd, use_peek != 0);
 }
