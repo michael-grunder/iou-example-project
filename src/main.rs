976fb7d2dc04287a2864243c24a9d60fb1ae1233
michael-grunder <michael.grunder@gmail.com>
2019-11-29 11:31:40 -0800

    Simplify example

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,9 @@ use std::{
     convert::TryInto,
     fs::File,
     io::{BufRead, BufReader, IoSlice},
-    marker::PhantomPinned,
     os::unix::io::AsRawFd,
     pin::Pin,
-    ptr::NonNull,
 };
-
-struct IoMarker<'a> {
-    _data: Vec<u8>,
-    iovec: Vec<IoSlice<'a>>,
-}
-
-impl<'a> IoMarker<'a> {
-    fn new(data: Vec<u8>) -> Self {
-        let data_ref = unsafe { &*(data.as_ref() as *const _) };
-
-        Self {
-            _data: data,
-            iovec: vec![IoSlice::new(data_ref)],
-        }
-    }
-
-    fn byte_len(&self) -> usize {
-        self._data.len()
-    }
-}
 
 struct IouMarker<'a> {
     _data: Vec<Vec<u8>>,
@@ -37,7 +15,7 @@ struct IouMarker<'a> {
 }
 
 impl<'a> IouMarker<'a> {
-    fn new<T>(writes: T) -> Pin<Box<Self>>
+    fn pinned<T>(writes: T) -> Pin<Box<Self>>
     where
         T: Into<Vec<Vec<u8>>>,
     {
@@ -62,7 +40,7 @@ impl<'a> IouMarker<'a> {
         Box::pin(container)
     }
 
-    fn new2<T>(writes: T) -> Self
+    fn raw<T>(writes: T) -> Self
     where
         T: Into<Vec<Vec<u8>>>,
     {
@@ -94,104 +72,55 @@ impl<'a> IouMarker<'a> {
     }
 }
 
-//fn zip2(f1: &str, f2: &str, qd: u32) {
-//    let rdr1 = BufReader::new(File::open(f1).expect("Can't open first file"));
-//    let rdr2 = BufReader::new(File::open(f2).expect("Can't open second file"));
-//
-//    let zfile = File::create("rust_testzip").expect("Can't open zip output file");
-//    let fd = zfile.as_raw_fd();
-//
-//    let mut iou = IoUring::new(qd).expect("Can't create ring");
-//    let mut map = HashMap::new();
-//
-//    let mut inflight = 0;
-//    let mut offset = 0;
-//    let mut id = 0;
-//
-//    for (l1, l2) in rdr1.lines().zip(rdr2.lines()) {
-//        if let (Ok(mut a), Ok(mut b)) = (l1, l2) {
-//            if inflight == qd - 1 {
-//                eprintln!("Pausing for {} inflight writes...", inflight);
-//                while inflight > 0 {
-//                    let cqe = iou.wait_for_cqe().unwrap();
-//                    map.remove(&cqe.user_data());
-//                    inflight -= 1;
-//                }
-//            }
-//
-//            a.push('\n');
-//            b.push('\n');
-//
-//            let ios = vec![IoMarker::new(a.into_bytes()), IoMarker::new(b.into_bytes())];
-//
-//            for io in ios.into_iter() {
-//                let mut sqe = iou.next_sqe().unwrap();
-//                sqe.set_user_data(id);
-//                unsafe {
-//                    sqe.prep_write_vectored(fd, &io.iovec, offset);
-//                    inflight += 1;
-//                    offset += io.byte_len();
-//                }
-//
-//                map.insert(id, io);
-//                id += 1;
-//            }
-//        }
-//    }
-//
-//    eprint!("Waiting for {} writes...", inflight);
-//    let _ = iou.wait_for_cqes(inflight.try_into().unwrap());
-//    eprintln!("done!");
-//}
-//
-//fn zip(f1: &str, f2: &str, qd: u32) {
-//    let rdr1 = BufReader::new(File::open(f1).expect("Can't open first file"));
-//    let rdr2 = BufReader::new(File::open(f2).expect("Can't open second file"));
-//
-//    let zfile = File::create("rust_testzip").expect("Can't open zip output file");
-//    let fd = zfile.as_raw_fd();
-//
-//    let mut iou = IoUring::new(qd).expect("Can't create ring");
-//    let mut map = HashMap::new();
-//
-//    let mut inflight = 0;
-//    let mut offset = 0;
-//    let mut id = 0;
-//
-//    for (l1, l2) in rdr1.lines().zip(rdr2.lines()) {
-//        if let (Ok(mut a), Ok(mut b)) = (l1, l2) {
-//            if inflight == qd {
-//                eprintln!("Pausing for {} inflight writes...", inflight);
-//                while inflight > 0 {
-//                    let _ = iou.wait_for_cqe();
-//                    inflight -= 1;
-//                }
-//            }
-//
-//            a.push('\n');
-//            b.push('\n');
-//
-//            let io = IouMarker::new(vec![a.into_bytes(), b.into_bytes()]);
-//
-//            let mut sqe = iou.next_sqe().unwrap();
-//            sqe.set_user_data(id);
-//            unsafe {
-//                sqe.prep_write_vectored(fd, &io.iovecs, offset);
-//                inflight += 1;
-//                offset += io.byte_len();
-//            }
-//
-//            map.insert(id, io);
-//            id += 1;
-//        }
-//    }
-//
-//    eprint!("Waiting for {} writes...", inflight);
-//    let _ = iou.wait_for_cqes(inflight.try_into().unwrap());
-//    eprintln!("done!");
-//}
+// Alternate use where we zip two files together line by line
+fn zip_lines(f1: &str, f2: &str, qd: u32) {
+    let rdr1 = BufReader::new(File::open(f1).expect("Can't open first file"));
+    let rdr2 = BufReader::new(File::open(f2).expect("Can't open second file"));
 
-fn copy(infile: &str, qd: u32) {
+    let zfile = File::create("rust_testzip").expect("Can't open zip output file");
+    let fd = zfile.as_raw_fd();
+
+    let mut iou = IoUring::new(qd).expect("Can't create ring");
+    let mut map = HashMap::new();
+
+    let mut inflight = 0;
+    let mut offset = 0;
+    let mut id = 0;
+
+    for (l1, l2) in rdr1.lines().zip(rdr2.lines()) {
+        if let (Ok(mut a), Ok(mut b)) = (l1, l2) {
+            if inflight == qd {
+                eprintln!("Pausing for {} inflight writes...", inflight);
+                while inflight > 0 {
+                    let _ = iou.wait_for_cqe();
+                    inflight -= 1;
+                }
+            }
+
+            a.push('\n');
+            b.push('\n');
+
+            let io = IouMarker::raw(vec![a.into_bytes(), b.into_bytes()]);
+
+            let mut sqe = iou.next_sqe().unwrap();
+            sqe.set_user_data(id);
+            unsafe {
+                sqe.prep_write_vectored(fd, &io.iovecs, offset);
+                inflight += 1;
+                offset += io.byte_len();
+            }
+
+            map.insert(id, io);
+            id += 1;
+        }
+    }
+
+    eprint!("Waiting for {} writes...", inflight);
+    let _ = iou.wait_for_cqes(inflight.try_into().unwrap());
+    eprintln!("done!");
+}
+
+fn iou_copy_lines(infile: &str, qd: u32) {
     let rdr = BufReader::new(File::open(infile).expect("Can't open input file"));
 
     let ofile = File::create("rust_testcopy").expect("Can't open output file");
@@ -217,7 +146,7 @@ fn copy(infile: &str, qd: u32) {
             }
         }
 
-        let io = IouMarker::new2(vec![line.into_bytes()]);
+        let io = IouMarker::raw(vec![line.into_bytes()]);
 
         let mut sqe = iou.next_sqe().unwrap();
         sqe.set_user_data(id);
@@ -230,7 +159,7 @@ fn copy(infile: &str, qd: u32) {
         map.insert(id, io);
         id += 1;
 
-        if id % 100000 == 0 {
+        if id % 1000 == 0 {
             eprintln!(
                 "[{}]: In-flight: {}, pauses: {}, offset: {}",
                 id, inflight, pauses, offset
@@ -244,74 +173,19 @@ fn copy(infile: &str, qd: u32) {
 }
 
 fn main() {
-    //let v: Vec<u8> = vec![1, 2, 3];
-    //let iom = IoMarker::new(v);
-
-    //println!("{:p}, {:p}", &iom._data, &iom.iovec[0]);
-
-    //let ioum = IouMarker::new(vec![vec![1, 2, 3]]);
-
     let args = std::env::args().collect::<Vec<_>>();
-    let f1 = &args[1];
 
-    let qd = if args.len() > 3 {
-        args[2].parse().unwrap()
+    let qd = if args.len() > 2 {
+        args[1].parse().unwrap()
     } else {
         128
     };
 
-    copy(f1, qd);
-}
+    let f1 = if args.len() > 3 {
+        &args[2]
+    } else {
+        "10k.lines"
+    };
 
-//fn main() {
-//    let infile = std::env::args().nth(1).expect("Pass an input file");
-//    let qd = std::env::args()
-//        .nth(2)
-//        .unwrap_or("127".to_string())
-//        .parse()
-//        .unwrap();
-//
-//    let rdr = BufReader::new(File::open(infile).expect("Can't open file"));
-//
-//    let file = File::create("rust_testfile").unwrap();
-//    let fd = file.as_raw_fd();
-//    let mut inflight = 0;
-//    let mut id = 0;
-//    let mut offset = 0;
-//
-//    println!("Queue depth: {}", qd);
-//
-//    let mut iou = IoUring::new(qd).expect("Can't create ring");
-//
-//    let mut map = HashMap::new();
-//
-//    for mut line in rdr.lines().filter_map(|l| l.ok()) {
-//        if inflight == qd {
-//            eprintln!("Pausing for {} inflight writes...", inflight);
-//            while inflight > 0 {
-//                let _ = iou.wait_for_cqe();
-//                inflight -= 1;
-//            }
-//        }
-//
-//        line.push('\n');
-//
-//        let io = IouMarker::new(vec![line.into_bytes()]);
-//
-//        let mut sqe = iou.next_sqe().unwrap();
-//        sqe.set_user_data(id);
-//        unsafe {
-//            sqe.prep_write_vectored(fd, &io.iovecs, offset);
-//            sqe.set_flags(SubmissionFlags::IO_LINK);
-//            inflight += 1;
-//            offset += io.byte_len();
-//        }
-//
-//        map.insert(id, io);
-//        id += 1;
-//    }
-//
-//    eprint!("Waiting for {} writes...", inflight);
-//    let _ = iou.wait_for_cqes(inflight.try_into().unwrap());
-//    eprintln!("done!");
-//}
+    iou_copy_lines(f1, qd);
+}
